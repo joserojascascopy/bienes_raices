402223233a7c8c561b7dc651ccc3d0e307a291aa
joserojascascopy <joserojascascopy@hotmail.com>
2024-10-01 22:46:57 -0400

    Se agrega el modo oscuro (boton y preferencia del usuario)

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() { // Esperamos a que cargue todo el DOM para ejecutar el codigo
     eventListeners();
+    darkModeUser();
 });
 
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu'); // Traemos la clase de .mobile-menu
+    const botonDarkMode = document.querySelector('.dark-mode');
 
     mobileMenu.addEventListener('click', navegacionResponsive) // Le agregamos un Event Listener de 'click' que ejecuta la función navegacionResponsive
+    botonDarkMode.addEventListener('click', darkMode)
 }
 
 function navegacionResponsive() {
@@ -21,3 +24,20 @@ function navegacionResponsive() {
 }
 
 // Una vez que se agrega (la clase .mostrar) a la lista de clases de navegacion, con css modificamos dicha clase para mostrar en el DO
+
+function darkMode() {
+    const body = document.querySelector('body');
+
+    body.classList.toggle('dark');
+}
+
+function darkModeUser() {
+    const preferenciaUsuario = window.matchMedia('(prefers-color-scheme: dark)');
+    const body = document.querySelector('body');
+
+    if(preferenciaUsuario.matches) { // Es un objeto, por lo tanto debemos acceder a la propiedad con .matches (boolean, true o false)
+        body.classList.add('dark');
+    }else {
+        body.classList.remove('dark');
+    }
+}
